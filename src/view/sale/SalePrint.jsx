d75cec744606e93326fce63cc2ab512ecc0b8514
Britz Wang <britz.wang@kooppi.com>
2021-06-08 23:28:25 +0800

    0608

--- src/view/sale/SalePrint.jsx
+++ src/view/sale/SalePrint.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Modal } from 'antd'
+import { Modal, Button } from 'antd'
 import SaleAPI from '../../model/api/sale'
 
 export default class SalePrint extends React.Component {
@@ -88,7 +88,10 @@ export default class SalePrint extends React.Component {
                 ></td>
                 <td colSpan='2'></td>
                 <td></td>
-                <td colSpan='4' style={{ textAlign: 'right', verticalAlign: 'top', padding: '9px 10px' }}>
+                <td
+                  colSpan='4'
+                  style={{ textAlign: 'right', verticalAlign: 'top', padding: '9px 10px' }}
+                >
                   {this.getPrice(printData[page].totalAmount)}
                 </td>
               </tr>
@@ -101,217 +104,227 @@ export default class SalePrint extends React.Component {
   }
 
   render() {
-    return this.state.printData.map((data, index) => (
-      <div key={index} className='print-area'>
-        <div className='print-backward'>
-          <div className='header'>
-            <div className='header-left'>
-              <div>客戶寶號：</div>
-              <div>客戶電話：</div>
-              <div>客戶地址：</div>
-            </div>
-            <div className='header-right'>
-              <div>頁數：</div>
-              <div>日期：</div>
-              <div>單號：</div>
-            </div>
-          </div>
-          <div className='table'>
-            <div className='table-right'>送貨</div>
-            <table className='table-table'>
-              <colgroup>
-                <col width='16' />
-                <col width='34' />
-                <col width='102' />
-                <col width='106' />
-                <col width='132' />
-                <col width='75' />
-                <col width='56' />
-                <col width='56' />
-                <col width='68' />
-                <col width='53' />
-                <col />
-              </colgroup>
-              <thead>
-                <tr>
-                  <th>序</th>
-                  <th colSpan='2'>商品代號</th>
-                  <th colSpan='2'>商品名稱</th>
-                  <th>規格</th>
-                  <th>數量</th>
-                  <th>單價</th>
-                  <th>金額</th>
-                  <th>備註</th>
-                  <th>顏色</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td colSpan='2'></td>
-                  <td colSpan='2'></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td colSpan='2'></td>
-                  <td colSpan='2'></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td colSpan='2'></td>
-                  <td colSpan='2'></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td colSpan='2'></td>
-                  <td colSpan='2'></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>5</td>
-                  <td colSpan='2'></td>
-                  <td colSpan='2'></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>6</td>
-                  <td colSpan='2'></td>
-                  <td colSpan='2'></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>7</td>
-                  <td colSpan='2'></td>
-                  <td colSpan='2'></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>8</td>
-                  <td colSpan='2'></td>
-                  <td colSpan='2'></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              </tbody>
-              <tfoot>
-                <tr>
-                  <th colSpan='2'>備註</th>
-                  <td colSpan='2'></td>
-                  <td colSpan='2'>正廠零件如不使用請即刻退回，並保持包裝完整。</td>
-                  <th>總計</th>
-                  <td colSpan='4'></td>
-                </tr>
-              </tfoot>
-            </table>
-          </div>
+    return (
+      <>
+        <div className='print-btn'>
+          <Button type='primary' onClick={() => window.print()}>
+            列印
+          </Button>
+          <Button onClick={() => window.close()}>關閉視窗</Button>
         </div>
-        <div className='print-forward'>
-          <div className='data-header'>
-            <div className='data-header-left'>
-              <div>{data.customerInfo}</div>
-              <div>{data.customerPhone}</div>
-              <div>{data.customerAddress}</div>
+        {this.state.printData.map((data, index) => (
+          <div key={index} className='print-area'>
+            <div className='print-backward'>
+              <div className='header'>
+                <div className='header-left'>
+                  <div>客戶寶號：</div>
+                  <div>客戶電話：</div>
+                  <div>客戶地址：</div>
+                </div>
+                <div className='header-right'>
+                  <div>頁數：</div>
+                  <div>日期：</div>
+                  <div>單號：</div>
+                </div>
+              </div>
+              <div className='table'>
+                <div className='table-right'>送貨</div>
+                <table className='table-table'>
+                  <colgroup>
+                    <col width='16' />
+                    <col width='34' />
+                    <col width='102' />
+                    <col width='106' />
+                    <col width='132' />
+                    <col width='75' />
+                    <col width='56' />
+                    <col width='56' />
+                    <col width='68' />
+                    <col width='53' />
+                    <col />
+                  </colgroup>
+                  <thead>
+                    <tr>
+                      <th>序</th>
+                      <th colSpan='2'>商品代號</th>
+                      <th colSpan='2'>商品名稱</th>
+                      <th>規格</th>
+                      <th>數量</th>
+                      <th>單價</th>
+                      <th>金額</th>
+                      <th>備註</th>
+                      <th>顏色</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td colSpan='2'></td>
+                      <td colSpan='2'></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td colSpan='2'></td>
+                      <td colSpan='2'></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td colSpan='2'></td>
+                      <td colSpan='2'></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td colSpan='2'></td>
+                      <td colSpan='2'></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>5</td>
+                      <td colSpan='2'></td>
+                      <td colSpan='2'></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>6</td>
+                      <td colSpan='2'></td>
+                      <td colSpan='2'></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>7</td>
+                      <td colSpan='2'></td>
+                      <td colSpan='2'></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>8</td>
+                      <td colSpan='2'></td>
+                      <td colSpan='2'></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <th colSpan='2'>備註</th>
+                      <td colSpan='2'></td>
+                      <td colSpan='2'>正廠零件如不使用請即刻退回，並保持包裝完整。</td>
+                      <th>總計</th>
+                      <td colSpan='4'></td>
+                    </tr>
+                  </tfoot>
+                </table>
+              </div>
             </div>
-            <div className='data-header-right'>
-              <div>{data.page}</div>
-              <div>{data.accountDate}</div>
-              <div>{data.salesId}</div>
+            <div className='print-forward'>
+              <div className='data-header'>
+                <div className='data-header-left'>
+                  <div>{data.customerInfo}</div>
+                  <div>{data.customerPhone}</div>
+                  <div>{data.customerAddress}</div>
+                </div>
+                <div className='data-header-right'>
+                  <div>{data.page}</div>
+                  <div>{data.accountDate}</div>
+                  <div>{data.salesId}</div>
+                </div>
+              </div>
+              <div className='data-table'>
+                <table>
+                  <colgroup>
+                    <col width='16' />
+                    <col width='34' />
+                    <col width='102' />
+                    <col width='106' />
+                    <col width='132' />
+                    <col width='75' />
+                    <col width='56' />
+                    <col width='56' />
+                    <col width='68' />
+                    <col width='53' />
+                    <col />
+                  </colgroup>
+                  <tbody>
+                    {data.details.map((record, row) => (
+                      <tr key={`table-${index}-${row}`} className='row'>
+                        <td></td>
+                        <td colSpan='2'>
+                          <div>{record.productId}</div>
+                        </td>
+                        <td colSpan='2'>
+                          <div>{record.product_desc}</div>
+                        </td>
+                        <td>
+                          <div>{record.norm}</div>
+                        </td>
+                        <td>
+                          <div>{record.quantity}</div>
+                        </td>
+                        <td>
+                          <div>{this.getPrice(record.price)}</div>
+                        </td>
+                        <td>
+                          <div>{this.getPrice(record.amount)}</div>
+                        </td>
+                        <td>
+                          <div>{record.remark}</div>
+                        </td>
+                        <td>
+                          <div>{record.color}</div>
+                        </td>
+                      </tr>
+                    ))}
+                    {this.generateEmptyNote(index)}
+                  </tbody>
+                </table>
+              </div>
+              {this.generateFoot(index)}
             </div>
           </div>
-          <div className='data-table'>
-            <table>
-              <colgroup>
-                <col width='16' />
-                <col width='34' />
-                <col width='102' />
-                <col width='106' />
-                <col width='132' />
-                <col width='75' />
-                <col width='56' />
-                <col width='56' />
-                <col width='68' />
-                <col width='53' />
-                <col />
-              </colgroup>
-              <tbody>
-                {data.details.map((record, row) => (
-                  <tr key={`table-${index}-${row}`} className='row'>
-                    <td></td>
-                    <td colSpan='2'>
-                      <div>{record.productId}</div>
-                    </td>
-                    <td colSpan='2'>
-                      <div>{record.product_desc}</div>
-                    </td>
-                    <td>
-                      <div>{record.norm}</div>
-                    </td>
-                    <td>
-                      <div>{record.quantity}</div>
-                    </td>
-                    <td>
-                      <div>{this.getPrice(record.price)}</div>
-                    </td>
-                    <td>
-                      <div>{this.getPrice(record.amount)}</div>
-                    </td>
-                    <td>
-                      <div>{record.remark}</div>
-                    </td>
-                    <td>
-                      <div>{record.color}</div>
-                    </td>
-                  </tr>
-                ))}
-                {this.generateEmptyNote(index)}
-              </tbody>
-            </table>
-          </div>
-          {this.generateFoot(index)}
-        </div>
-      </div>
-    ))
+        ))}
+      </>
+    )
   }
 }
